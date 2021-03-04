b388c2a2fcad7ecc5c7a3cf7632926d957c8f187
AnyJuanElse <jupasava17@gmail.com>
2021-03-04 17:18:40 -0500

    versión final

---
+++ JS/effect.js
@@ -0,0 +1,11 @@
+
+var marcado = document.getElementById('menu-bar');
+
+function img () {
+if (marcado.checked == true)
+{
+    document.getElementById('img_effect').style.visibility="hidden";
+} else {
+    document.getElementById('img_effect').style.visibility="visible";
+}
+}

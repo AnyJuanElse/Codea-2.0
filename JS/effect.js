6024a559e0326df1c90c61aea6b7897c3c5a1150
AnyJuanElse <jupasava17@gmail.com>
2021-03-05 12:48:10 -0500

    Responsive

--- JS/effect.js
+++ JS/effect.js
@@ -1,11 +1,6 @@
 
-var marcado = document.getElementById('menu-bar');
+let fadeOut = document.getElementById('img_effect');
 
-function img () {
-if (marcado.checked == true)
-{
-    document.getElementById('img_effect').style.visibility="hidden";
-} else {
-    document.getElementById('img_effect').style.visibility="visible";
-}
+document.getElementById('menu-bar').onclick = function(){
+    fadeOut.classList.toggle('fade');
 }
